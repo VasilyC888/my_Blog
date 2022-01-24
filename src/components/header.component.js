a882cbddb53b1c45c23fe3001ef13fbf7dc338f8
VasilyC888 <kasoi129@gmail.com>
2022-01-24 23:43:36 +0200

    add files

---
+++ src/components/header.component.js
@@ -0,0 +1,20 @@
+import {Component} from '../core/component'
+
+export class HeaderComponent extends Component {
+    constructor(id) {
+        super(id)
+
+    }
+
+    init() {
+        this.$el.querySelector('.js-header-start').addEventListener('click', buttonHandler.bind(this))
+
+
+
+    }
+
+}
+
+function buttonHandler() {
+    this.hide()
+}
